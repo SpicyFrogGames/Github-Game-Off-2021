f7b196ee47dd1f0f117f2c6b7ffd717c7c5572ba
Dioxymore <metalfox.dregsofsorrows@gmail.com>
2021-11-03 01:39:13 +0100

    Disable jump for rolling louse and some layers modifications

--- Assets/Scripts/Actions/WoodlouseTransformation.cs
+++ Assets/Scripts/Actions/WoodlouseTransformation.cs
@@ -7,17 +7,21 @@ public class WoodlouseTransformation : MonoBehaviour
 {
     public GameObject normalForm;
     public GameObject ballForm;
-    public float normalSpeed;
-    public float normalAcceleration;
     public float ballSpeed;
     public float ballAcceleration;
 
     private HorizontalMovement movement;
+    private Jump jump;
     private bool isBall = false;
+    private float normalSpeed;
+    private float normalAcceleration;
     
     void Start()
     {
+        jump = GetComponent<Jump>();
         movement = GetComponent<HorizontalMovement>();
+        normalSpeed = movement.maximumSpeed;
+        normalAcceleration = movement.acceleration;
     }
 
     void OnAction(InputValue value)
@@ -26,19 +30,23 @@ public class WoodlouseTransformation : MonoBehaviour
         {
             if (isBall)
             {
+                // Transforms to normal
                 normalForm.SetActive(true);
                 ballForm.SetActive(false);
                 isBall = false;
                 movement.maximumSpeed = normalSpeed;
                 movement.acceleration = normalAcceleration;
+                jump.enabled = true;
             }
             else
             {
+                // Transforms to ball
                 normalForm.SetActive(false);
                 ballForm.SetActive(true);
                 isBall = true;
                 movement.maximumSpeed = ballSpeed;
                 movement.acceleration = ballAcceleration;
+                jump.enabled = false;
             }
         }
     }
