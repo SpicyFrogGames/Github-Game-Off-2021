69f01a3536f5a7ff396891063d9d68bf43954396
Maksime <maxime.fieffe@gmail.com>
2021-11-16 21:52:26 +0100

    Finish first main menu version

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,43 +1,82 @@
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+namespace UI
 {
-    public Transform mainMenu;
-    public Transform optionsPanel;
-    public Transform creditsPanel;
+    public class MainMenu : MonoBehaviour
+    {
+        int activePanel = 0;
     
-    public void ShowMainMenu(Transform menuToHide)
-    {
-        mainMenu.DOLocalMoveX(0, 1f);
-    }
+        public Transform mainMenu;
+        public Transform optionsPanel;
+        public Transform creditsPanel;
 
-    public void ShowOptionsMenu()
-    {
-        mainMenu.DOLocalMoveX(-1000, 0.5f);
-        optionsPanel.DOLocalMoveX(0, 0.5f);
-    }
+        public Transform[] panels;
 
-    public void HideOptionsMenu()
-    {
-        optionsPanel.DOLocalMoveX(1000, 0.5f);
-        mainMenu.DOLocalMoveX(0, 0.5f);
-    }
+        public void ShowOptionsMenu()
+        {
+            mainMenu.DOLocalMoveX(-1000, 0.5f);
+            optionsPanel.DOLocalMoveX(0, 0.5f);
+        }
 
-    public void ShowCreditsMenu()
-    {
-        mainMenu.DOLocalMoveX(-1000, 0.5f);
-        creditsPanel.DOLocalMoveX(0, 0.5f);
-    }
+        public void HideOptionsMenu()
+        {
+            optionsPanel.DOLocalMoveX(1000, 0.5f);
+            mainMenu.DOLocalMoveX(0, 0.5f);
+        }
 
-    public void HideCreditsMenu()
-    {
-        creditsPanel.DOLocalMoveX(1000, 0.5f);
-        mainMenu.DOLocalMoveX(0, 0.5f);
-    }
+        public void ShowCreditsMenu()
+        {
+            mainMenu.DOLocalMoveX(-1000, 0.5f);
+            creditsPanel.DOLocalMoveX(0, 0.5f);
+            InitCreditsMenu();
+        }
 
-    public void QuitGame()
-    {
-        Application.Quit();
+        public void HideCreditsMenu()
+        {
+            ResetCreditsMenu();
+            creditsPanel.DOLocalMoveX(1000, 0.5f);
+            mainMenu.DOLocalMoveX(0, 0.5f);
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+
+        public void StartGame()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        void InitCreditsMenu()
+        {
+            panels[0].DOLocalMoveX(0, 0f);
+        }
+
+        void ResetCreditsMenu()
+        {
+            foreach (Transform panel in panels)
+            {
+                panel.DOLocalMoveX(800, .5f);
+            }
+
+            activePanel = 0;
+        }
+
+        public void NextSlide()
+        {
+            panels[activePanel].DOLocalMoveX(800, .5f);
+            
+            activePanel++;
+
+            if (activePanel > panels.Length - 1)
+            {
+                activePanel = 0;
+            }
+            
+            panels[activePanel].DOLocalMoveX(0, .5f);
+        }
     }
 }
